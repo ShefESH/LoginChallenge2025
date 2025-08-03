a447964030d678688ac2c7cb60379cefa1567bf0
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-03 14:16:38 +0100

    Refactored to remove JWT as no longer working as of 3.8.2025 - changed to simple cookie value editing.

--- web/app.py
+++ web/app.py
@@ -2,10 +2,6 @@ from flask import Flask
 from flask import render_template, jsonify, make_response, redirect, request, session, render_template_string
 from flask_bootstrap import Bootstrap
 
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
 from numpy import identity
 
 app = Flask(__name__)
@@ -21,12 +17,11 @@ app.secret_key = "pinrg8gns#arjg;/-]]"
 app.config["JWT_SECRET_KEY"] = ";nod87b;/dfub6vaz.knib"
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 
-jwt = JWTManager(app)
 
 @app.route("/")
 def index():
     success = request.args.get("loginSuccessful")
-    if success is not None and success == "True":
+    if success is not None and success.lower() == "true":
         return render_template("hacked.html")
     elif success is not None:
         return render_template('home.html', login="Failed")
@@ -42,10 +37,8 @@ def verify_login():
 
 @app.route("/hungry")
 def hungry():
-    id_dict = {'is_admin': False}
-    access_token = create_access_token(identity=id_dict)
     resp = make_response(render_template("hungry.html"))
-    resp.set_cookie('access_token_cookie', access_token)
+    resp.set_cookie('is_admin', "false")
     return resp
 
 @app.route("/who-are-you", methods=["POST"])
@@ -58,12 +51,11 @@ def get_name():
         return redirect("/cookie-jar")
 
 @app.route("/cookie-jar")
-@jwt_required()
 def cookie_jar():
-    identity = get_jwt_identity()
     name = session['name']
+    is_admin = request.cookies.get('is_admin', "false")
 
-    if identity['is_admin']:
+    if is_admin == "true":
         welcome = "Congratulations! You win a laptop sticker!"
         return render_template("cookie-jar.html", welcome_msg=welcome, win=True)
     else:
@@ -79,3 +71,7 @@ def cleanup():
     resp.set_cookie('access_token_cookie', '', expires=0)
     resp.set_cookie('Authorization', '', expires=0)
     return resp
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2346)
